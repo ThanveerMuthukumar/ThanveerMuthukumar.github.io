3afab7f71d6943d9218aed505b196b16c87b8dcb
Thanveer Muthukumar <thanveermuthukumar@gmail.com>
2024-12-28 18:35:07 -0600

    Init commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer>
+            <h2>Thanveer Muthukumar</h2>
+            <p>made with React</p>
+            <img src="assets/react.png" alt="React Logo" />
+        </footer>
+    );
+}
+
+export default Footer;
